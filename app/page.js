2747f2dc7628bd4c29b5b1e313eae970b839a95b
kp1153 <kamta_tiwari@yahoo.com>
2025-11-19 18:20:24 +0530

    force deploy

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
-import HeroSection from "../components/HeroSection";
-import AboutSection from "../components/AboutSection";
-import FounderSection from "../components/FounderSection";
-import ProgramsSection from "../components/ProgramsSection";
-import ImpactSection from "../components/ImpactSection";
-import DonateSection from "../components/DonateSection";
-import ContactSection from "../components/ContactSection";
-
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import FounderSection from "@/components/FounderSection";
+import ProgramsSection from "@/components/ProgramsSection";
+import ImpactSection from "@/components/ImpactSection";
+import DonateSection from "@/components/DonateSection";
+import ContactSection from "@/components/ContactSection";
 
 export default function Home() {
   return (
